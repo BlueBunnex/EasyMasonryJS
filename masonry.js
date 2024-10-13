6daaf54634102ce735918c5d3d1ee9e7556ba3dc
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-10-13 09:02:51 -0400

    Create masonry.js

---
+++ masonry.js
@@ -0,0 +1,34 @@
+function updateMasonry(containerId, imageMaxHeight) {
+        
+    const container      = document.getElementById(containerId);
+    const containerWidth = container.offsetWidth - 1; // minus one to avoid rounding errors
+    const images         = container.querySelectorAll("img");
+
+    var i = 0;
+
+    // process rows
+    while (i < images.length) {
+        
+        const imageRowCount = Math.ceil(containerWidth / imageMaxHeight); // calculate from imageMaxHeight parameter
+        
+        var aspectRatioSum = 0;
+
+        for (let j = i; j < i + imageRowCount; j++) {
+
+            if (j < images.length) {
+                aspectRatioSum += images[j].naturalWidth / images[j].naturalHeight;
+            } else {
+                aspectRatioSum += 1;
+            }
+        }
+
+        const height = containerWidth / aspectRatioSum;
+
+        for (let j = i; j < Math.min(i + imageRowCount, images.length); j++) {
+            
+            images[j].style.width = height * (images[j].naturalWidth / images[j].naturalHeight) + "px";
+        }
+
+        i += imageRowCount;
+    }
+}
