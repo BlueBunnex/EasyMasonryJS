48c932e21bf2c57b2e2dc769588ab3ced54d7e91
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2024-10-13 09:25:20 -0400

    Update masonry.js

--- masonry.js
+++ masonry.js
@@ -16,7 +16,15 @@ function updateMasonry(containerId, imageMaxHeight) {
         for (let j = i; j < i + imageRowCount; j++) {
 
             if (j < images.length) {
-                aspectRatioSum += images[j].offsetWidth / images[j].offsetHeight;
+
+                // store initial dimensions as data
+                if (images[j].dataset.initWidth == null) {
+                    
+                    images[j].dataset.initWidth  = images[j].offsetWidth;
+                    images[j].dataset.initHeight = images[j].offsetHeight;
+                }
+                
+                aspectRatioSum += images[j].dataset.initWidth / images[j].dataset.initHeight;
             } else {
                 aspectRatioSum += 1;
             }
@@ -26,7 +34,7 @@ function updateMasonry(containerId, imageMaxHeight) {
 
         for (let j = i; j < Math.min(i + imageRowCount, images.length); j++) {
             
-            images[j].style.width = height * (images[j].offsetWidth / images[j].offsetHeight) + "px";
+            images[j].style.width = height * (images[j].initWidth / images[j].initHeight) + "px";
         }
 
         i += imageRowCount;
