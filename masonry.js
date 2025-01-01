8247e8a7c0e7cf37a471d7f34b8a59b9e0f12b45
blue🐰 <75032462+BlueBunnex@users.noreply.github.com>
2025-01-01 11:21:07 -0500

    Update masonry.js

--- masonry.js
+++ masonry.js
@@ -1,19 +1,19 @@
-function initMasonryContainerById(containerId, doDebug = false) {
+async function initMasonryContainerById(containerId, doDebug = false) {
 
-    initMasonryContainerElement(document.getElementById(containerId), doDebug);
+    await initMasonryContainerElement(document.getElementById(containerId), doDebug);
 }
 
 // remember to put every element inside a div set to style="display: flex;"
 
 // layer system, where each layer is initialized and then items are shifted between them per the updater?
 
-function initMasonryContainerElement(container, doDebug = false) {
+async function initMasonryContainerElement(container, doDebug = false) {
 
     if (doDebug) {
         console.log("Entered function 'updateMasonryElement'");
     }
     
-    const items = container.querySelectorAll(".item");
+    const items = container.children;
 
     if (doDebug) {
         console.log("Item count: " + items.length);
@@ -23,6 +23,19 @@ function initMasonryContainerElement(container, doDebug = false) {
      * Process each item.
      */
     for (let i = 0; i < items.length; i++) {
+
+        /*
+         * Wait for image to load (or fail to) before continuing.
+         * Otherwise, their sizes won't load and we cannot update
+         * the masonry properly.
+         */
+        if (items[i].tagName === "IMG") {
+            await new Promise(res => {
+                if (element.complete) { return res(); }
+                element.onload = () => res();
+                element.onerror = () => res();
+            });
+        }
 
         // get initial width/height
         let width, height;
